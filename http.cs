1d7ebebf87e608c892f05e15279e7054b3c6ba10
Matthew Cavallo <macavall@microsoft.com>
2024-06-14 20:01:03 -0500

    frist

--- http.cs
+++ http.cs
@@ -5,28 +5,6 @@ using Microsoft.Extensions.Logging;
 
 namespace eventhubfamain
 {
-    //public class http
-    //{
-    //    private readonly ILogger<http> _logger;
-    //    private readonly IEHService ehService;
-
-    //    public http(ILogger<http> logger, IEHService _ehService)
-    //    {
-    //        _logger = logger;
-    //        ehService = _ehService;
-    //    }
-
-    //    [Function("http")]
-    //    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-    //    {
-    //        _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-    //        ehService.SendEvent(15);
-
-    //        return new OkObjectResult(ehService.GetStats());
-    //    }
-    //}
-
     public class httpStart
     {
         private readonly ILogger<httpStart> _logger;
